5bd38fb75dc2c49916ebe22f4b01350138e6816a
Marcus Griep <marcus@griep.us>
2022-10-30 20:59:03 -0600

    docs: explain new feature flags

--- aliri_oauth2/src/lib.rs
+++ aliri_oauth2/src/lib.rs
@@ -2,6 +2,19 @@
 //!
 //! This module uses the definition of OAuth2 as defined in
 //! [RFC 6749](https://datatracker.ietf.org/doc/html/rfc6749).
+//!
+//! # Feature flags
+//!
+//! When using this crate and the `reqwest` feature to enable
+//! automatic background refreshing of JWKS, this crate does
+//! not automatically enable TLS support in `reqwest` itself.
+//! If your application already uses `reqwest` with some TLS
+//! settings (native/OpenSSL/rustls), then this crate will
+//! use those settings automatically. However, if the only
+//! reason you are using `reqwest` is transitively through
+//! this crate, you may need to enable the `default-tls` or
+//! `rustls-tls` feature to enable support for calling out to
+//! an HTTPS endpoint.
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![warn(
